e0751cefb26e74ab848037a572ddfb8366a2796c
zheli <zheli5@illinois.edu>
2018-12-01 19:50:51 -0600

    first commit

--- app/src/main/java/com/example/daniel/stocktracker/MainActivity.java
+++ app/src/main/java/com/example/daniel/stocktracker/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.daniel.stocktracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMenuPage();
+            }
+
+        });
     }
+    public void openMenuPage() {
+        Intent intent = new Intent(this, MenuPage.class);
+        startActivity(intent);
+    }
+
 }
